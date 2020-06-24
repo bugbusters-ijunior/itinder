23e9cb9d1ff527d68f85a984f9f3f4c5cec5c97d
Matheus Flávio <flavio.matheusgsilva@gmail.com>
2020-06-24 16:33:20 -0300

    Rotas para outras páginas

--- app/main/views.py
+++ app/main/views.py
@@ -11,7 +11,8 @@ def load_user(user_id):
 @app.route('/')
 @login_required
 def teste():
-    return render_template('teste.html')
+    associacao = Association.query.all()
+    return render_template('teste.html', associacao = associacao)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -31,3 +32,42 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("login"))    
+
+@app.route('/like_diretoria/<id>', methods=['POST','GET'])
+def like_diretoria(id):
+    trainee = Trainee.query.get_or_404(id)
+    diretoria = Diretoria.query.all()
+
+    if request.method == 'POST':
+        id_diretoria = request.form['id_diretoria']
+        diretoria = Diretoria.query.get_or_404(id_diretoria)
+        trainee.diretoria.append(diretoria)
+
+        db.session.commit()
+
+        return redirect(url_for('app.index'))
+
+    return render_template('like_diretoria.html', trainee = trainee, diretoria = diretoria)
+
+@app.route('/cadastro_trainee', methods = ['POST','GET'])
+def cadastro_trainee():
+    if request.method == 'POST':
+        email = request.form['email']
+        nome = request.form['nome']
+        senha = request.form['senha']
+
+        trainee = Trainee(email = email,
+                          nome = nome,
+                          senha = senha)
+        
+        db.session.add(trainee)
+        db.session.commit()
+
+        return redirect(url_for(app.index))
+    return render_template('cadastrar_trainee.html')
+
+@app.route('/perfil/<id>')
+def perfil(id):
+    trainee = Trainee.query.get_or_404(id)
+
+    return render_template('perfil_trainee.html', trainee = trainee)
